45dc585a49e4b0d56f8982e12ae623891f3d0fd1
xavier <xavierlesa@gmail.com>
2015-06-05 14:34:19 -0300

    fix admin list

--- djangometrics/admin.py
+++ djangometrics/admin.py
@@ -4,5 +4,9 @@ from django.contrib import admin
 from djangometrics.models import DjangoMetric
 
 class DjangoMetricAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+            'tag_name',
+            'content_type',
+            'object_id',
+            )
 admin.site.register(DjangoMetric, DjangoMetricAdmin)
